b030ad02108839bc9217e762993404fcc7385bab
sivaAuto <sivaprasad.rayapathi@gmail.com>
2017-01-26 20:17:49 +0530

    changes in commited Demo file

---
+++ Demo.java
@@ -0,0 +1,22 @@
+class Demo
+
+{
+  
+   public static void main (String args[])
+
+         {
+           
+             
+
+
+           for(int i=1;i<=10;i++)
+               {
+                   
+           System.out.println("Demo programm",+i); 
+                }
+
+
+
+          }
+
+}
